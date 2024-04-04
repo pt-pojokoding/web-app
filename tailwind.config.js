392474876d81df86bc8a2591505eb269adff0ccc
omanfathurrahman <oman.fathur.rahman.nur@outlook.com>
2024-04-04 13:45:26 +0700

    extend aspect ratio to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ export default {
         primary: '#865DFF',
         second: '#E384FF',
         bg: '#191825'
+      },
+      aspectRatio: {
+        auto: 'auto',
+        square: '1 / 1',
+        video: '16 / 9'
       }
     }
   },
