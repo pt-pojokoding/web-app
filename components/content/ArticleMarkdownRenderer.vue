3c3a20eb76349f6b0fd79bfaf7915be8fe820d3c
omanfathurrahman <oman.fathur.rahman.nur@outlook.com>
2024-04-25 23:53:44 +0700

    styling artikel

--- components/content/ArticleMarkdownRenderer.vue
+++ components/content/ArticleMarkdownRenderer.vue
@@ -4,7 +4,6 @@ import { renderMarkdown } from '~/services/content/renderMarkdown';
 const useContent = useContentStore();
 const markdown = useContent.currentContent.markdown;
 const renderedMarkdown = renderMarkdown(markdown);
-
 </script>
 
 <template>
@@ -15,10 +14,46 @@ const renderedMarkdown = renderMarkdown(markdown);
 
 <style lang="postcss">
 .toc-container {
-    @apply dark:bg-slate-800 border shadow-sm p-4 my-4 rounded
+    @apply dark:bg-slate-800 border shadow-sm p-4 my-4 rounded absolute right-8 top-20 w-4/12
 }
 
 .toc-link {
     @apply hover:text-primary text-lg
+}
+
+.article {
+    @apply py-4
+}
+
+.article h1, .article h2 {
+    @apply mt-5 mb-2
+}
+
+.article p {
+    @apply my-2 text-justify
+}
+
+.article table td, .article table th {
+    @apply table-fixed border-solid border px-2
+}
+
+.article ul {
+    @apply list-disc list-inside
+}
+
+.article ol {
+    @apply list-decimal list-inside
+}
+
+.article h1 {
+    @apply text-2xl
+}
+
+.article h2 {
+    @apply text-xl font-semibold
+}
+
+.article pre {
+    @apply bg-slate-800/60 text-white p-2 rounded
 }
 </style>
